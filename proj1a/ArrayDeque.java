3d268744aed9ed4208fe4ac056532019a3a27d03
Wsgamer7 <ws18022@outlook.com>
2022-10-08 13:16:59 +0800

    elegant ArrayDeque

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,17 +1,24 @@
 public class ArrayDeque<T> {
+    /** the size of the ArrayDeque */
     private int size;
+    /** the index of the first item in array */
     private int front;
+    /** the index after the last item */
     private int last;
+    /** the inner Array */
     private T[] lst;
+    /** the instantiation of class ArrayDeque, result in a empty Array (length = 8) */
     public ArrayDeque() {
         lst = (T[]) new Object[8];
         size = 0;
         front = 0;
         last = 0;
     }
+    /** the useage of the inner Array */
     private float useage() {
         return (float) size / lst.length;
     }
+    /** minusOne in circus list, replace i-- */
     private int minusOne(int index) {
         index = index - 1;
         if (index < 0) {
@@ -19,9 +26,11 @@ public class ArrayDeque<T> {
         }
         return index;
     }
+    /** plusOne in circus list, replace i++ */
     private int plusOne(int index) {
         return (index + 1) % lst.length;
     }
+    /** resizing the inner Array, take the length of new inner Array */
     private void resizing(int n) {
         T[] newLst = (T[]) new Object[n];
         for (int i = 0; i < size; i += 1) {
@@ -30,30 +39,27 @@ public class ArrayDeque<T> {
         }
         lst = newLst;
         front = 0;
-        last = size - 1;
+        last = size;
     }
+    private void resizingInArrayDeque() {
+        resizing(Math.max(8, size * 2));
+    }
+    /** add x to the first position of the ArrayDeque */
     public void addFirst(T x) {
-        if (lst.length - size < 1) {
-            resizing(2 * size);
+        if (lst.length - size < 2) {
+            resizingInArrayDeque();
         }
-        if (isEmpty()) {
-            lst[front] = x;
-        } else {
-            front = minusOne(front);
-            lst[front] = x;
-        }
+        front = minusOne(front);
+        lst[front] = x;
         size += 1;
     }
+    /** add x to the last position of the ArrayDeque */
     public void addLast(T x) {
-        if (lst.length - size < 1) {
-            resizing(2 * size);
+        if (lst.length - size < 2) {
+            resizingInArrayDeque();
         }
-        if (isEmpty()) {
-            lst[front] = x;
-        } else {
-            last = plusOne(last);
-            lst[last] = x;
-        }
+        lst[last] = x;
+        last = plusOne(last);
         size += 1;
     }
     public boolean isEmpty() {
@@ -62,42 +68,40 @@ public class ArrayDeque<T> {
     public int size() {
         return size;
     }
+    /** print out the Deque */
     public void printDeque() {
         for (int i = front; i != last; i = plusOne(i)) {
             System.out.print(i);
             System.out.print(" ");
         }
-        System.out.print(last);
-        System.out.print(" ");
     }
+    /** remove the first item */
     public T removeFirst() {
         if (isEmpty()) {
             return null;
         }
         T shouldReturn = lst[front];
-        if (size != 1) {
-            front = plusOne(front);
-        }
+        front = plusOne(front);
         size -= 1;
         if (useage() < 0.25 && lst.length >= 16) {
-            resizing(Math.max(2 * size, 8));
+            resizingInArrayDeque();
         }
         return shouldReturn;
     }
+    /** remove the last item */
     public T removeLast() {
         if (isEmpty()) {
             return null;
         }
+        last = minusOne(last);
         T shouldReturn = lst[last];
-        if (size != 1) {
-            last = minusOne(last);
-        }
         size -= 1;
         if (useage() < 0.25 && lst.length >= 16) {
-            resizing(Math.max(2 * size, 8));
+            resizingInArrayDeque();
         }
         return shouldReturn;
     }
+    /** return the indexTH item */
     public T get(int index) {
         if (index > size - 1) {
             return null;
