8649f238fd48c1171a45d18539b8bebdf1f8f750
Wsgamer7 <ws18022@outlook.com>
2022-10-10 20:04:32 +0800

    test

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -2,21 +2,22 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TestOffByOne {
+    /*
     // You must use this CharacterComparator and not instantiate
-    // new ones, or the autograder might be upset.
+    // new ones, or the autograder might be upset. */
     static CharacterComparator offByOne = new OffByOne();
 
-    // Your tests go here.
+    /*Your tests go here.
+    Uncomment this class once you've created your
+    CharacterComparator interface and OffByOne class. **/
     @Test
     public void testEqualChars() {
-        assertTrue(offByOne.equalChars('a', 'b'));
-        assertTrue(offByOne.equalChars('r', 'q'));
-        assertTrue(offByOne.equalChars('&', '%'));
-
-        assertFalse(offByOne.equalChars('a', 'a'));
-        assertFalse(offByOne.equalChars('a', 'z'));
-        assertFalse(offByOne.equalChars('a', 'e'));
-        assertFalse(offByOne.equalChars('a', 'A'));
+        boolean t1 = offByOne.equalChars('a', 'b');
+        boolean t2 = offByOne.equalChars('r', 'q');
+        boolean f1 = offByOne.equalChars('a', 'a');
+        boolean f2 = offByOne.equalChars('a', 'B');
+        boolean f3 = offByOne.equalChars('a', 'z');
+        assertTrue(t1 && t2);
+        assertFalse(f1 && f2 && f3);
     }
-
 }
--- proj1b/TestPalindrome.java
+++ proj1b/TestPalindrome.java
@@ -1,36 +1,35 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
-
 public class TestPalindrome {
-    // You must use this palindrome, and not instantiate
-    // new Palindromes, or the autograder might be upset.
+    /*// You must use this palindrome, and not instantiate
+    // new Palindromes, or the autograder might be upset.*/
     static Palindrome palindrome = new Palindrome();
 
     @Test
     public void testWordToDeque() {
-        Deque d = palindrome.wordToDeque("persiflage");
+        Deque<Character> d = palindrome.wordToDeque("persiflage");
         String actual = "";
         for (int i = 0; i < "persiflage".length(); i++) {
             actual += d.removeFirst();
         }
         assertEquals("persiflage", actual);
     }
-
     @Test
     public void testIsPalindrome() {
-        assertTrue(palindrome.isPalindrome(""));
-        assertTrue(palindrome.isPalindrome("a"));
-        assertTrue(palindrome.isPalindrome("aba"));
-        assertTrue(palindrome.isPalindrome("aaccbbbccaa"));
+        String wordA1 = "nooon";
+        String wordA2 = "a";
+        assertTrue(palindrome.isPalindrome(wordA1) && palindrome.isPalindrome(wordA2));
 
-        assertFalse(palindrome.isPalindrome("ab"));
-        assertFalse(palindrome.isPalindrome("ababba"));
+        String wordB1 = "wang";
+        String wordB2 = "Aa";
+        assertFalse(palindrome.isPalindrome(wordB1) && palindrome.isPalindrome(wordB2));
     }
-
     @Test
-    public void testIsOffByOnePalindrome() {
+    public void testIsPalindromeGeneralized() {
+        String wordT1 = "flake";
+        String wordF1 = "wang";
         CharacterComparator cc = new OffByOne();
-        assertTrue(palindrome.isPalindrome("flake", cc));
-        assertFalse(palindrome.isPalindrome("aba", cc));
+        assertTrue(palindrome.isPalindrome(wordT1, cc));
+        assertFalse(palindrome.isPalindrome(wordF1, cc));
     }
 }
