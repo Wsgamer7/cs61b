691153eb3ce84f625cb6587d03be0e7ec5d780d4
Wsgamer7 <ws18022@outlook.com>
2022-10-16 19:29:51 +0800

    autograter

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -3,45 +3,41 @@ import org.junit.Test;
 public class TestArrayDequeGold {
     @Test
     public void randomTest() {
-        int timesOfTest = 100;
-        for (int i = 0; i < timesOfTest; i += 1) {
-            int maxCallTimes = 4;
-            String log = "";
-            StudentArrayDeque<Integer> stdAD = new StudentArrayDeque<Integer>();
-            ArrayDequeSolution<Integer> solAD = new ArrayDequeSolution<Integer>();
-            int actual = 0;
-            int expected = 0;
-            for (int j = 0; j < maxCallTimes; j += 1) {
-                int indexCalled = StdRandom.uniform(4);
-                if (solAD.isEmpty()) {
-                    indexCalled = StdRandom.uniform(2);
-                }
-                switch (indexCalled) {
-                    case 0:
-                        log = log + "addFirst(" + j + ")\n";
-                        stdAD.addFirst(j);
-                        solAD.addFirst(j);
-                        break;
-                    case 1:
-                        log = log + "addLast(" + j + ")\n";
-                        stdAD.addLast(j);
-                        solAD.addLast(j);
-                        break;
-                    case 2:
-                        log = log + "removeFirst()\n";
-                        actual = stdAD.removeFirst();
-                        expected = solAD.removeFirst();
-                        break;
-                    case 3:
-                        log = log + "removeLast()\n";
-                        actual = stdAD.removeLast();
-                        expected = solAD.removeLast();
-                        break;
-                }
-                if (actual != expected) {
-                    assertEquals(log, actual, expected);
-                }
+        int maxCallTimes = 100;
+        String log = "";
+        StudentArrayDeque<Integer> stdAD = new StudentArrayDeque<Integer>();
+        ArrayDequeSolution<Integer> solAD = new ArrayDequeSolution<Integer>();
+        int actual = 0;
+        int expected = 0;
+        for (int j = 0; j < maxCallTimes; j += 1) {
+            int indexCalled = StdRandom.uniform(4);
+            if (stdAD.isEmpty() | solAD.isEmpty()) {
+                indexCalled = StdRandom.uniform(2);
             }
+            switch (indexCalled) {
+                case 0:
+                    log = log + "addFirst(" + j + ")\n";
+                    stdAD.addFirst(j);
+                    solAD.addFirst(j);
+                    break;
+                case 1:
+                    log = log + "addLast(" + j + ")\n";
+                    stdAD.addLast(j);
+                    solAD.addLast(j);
+                    break;
+                case 2:
+                    log = log + "removeFirst()\n";
+                    actual = stdAD.removeFirst();
+                    expected = solAD.removeFirst();
+                    break;
+                case 3:
+                    log = log + "removeLast()\n";
+                    actual = stdAD.removeLast();
+                    expected = solAD.removeLast();
+                    break;
+                default:
+            }
+            assertEquals(log, actual, expected);
         }
     }
 }
