c5bec0d1067fa43a8a51b253628dfa9f6dec588e
Wsgamer <ws18022@outlook.com>
2023-04-07 16:31:04 +0800

    apart I, Rasterer

--- proj3/src/main/java/Rasterer.java
+++ proj3/src/main/java/Rasterer.java
@@ -8,9 +8,10 @@ import java.util.Map;
  * not draw the output correctly.
  */
 public class Rasterer {
-
-    public Rasterer() {
+    private final double lonDPP0;
+    public Rasterer(){
         // YOUR CODE HERE
+         this.lonDPP0 = lonDPP(MapServer.ROOT_ULLON, MapServer.ROOT_LRLON, MapServer.TILE_SIZE);
     }
 
     /**
@@ -44,9 +45,108 @@ public class Rasterer {
     public Map<String, Object> getMapRaster(Map<String, Double> params) {
         // System.out.println(params);
         Map<String, Object> results = new HashMap<>();
-        System.out.println("Since you haven't implemented getMapRaster, nothing is displayed in "
-                           + "your browser.");
+        //extract args from param and check it
+        double ullon = params.get("ullon");
+        double lrlon = params.get("lrlon");
+        double ullat = params.get("ullat");
+        double lrlat = params.get("lrlat");
+        double width = params.get("w");
+        if (inMap(ullon, ullat) && inMap(lrlon, lrlat)) { // checkout
+            results.put("query_success", false);
+            return results;
+        }
+        results.put("query_success", true);
+
+        //get good depth
+        int depth = bestDepth(ullon, lrlon, width);
+
+        //select what images we need
+        int[] leftUpTile = whichTile(ullon, ullat, depth);
+        int[] rightLowTile = whichTile(lrlon, lrlat, depth);
+        String[][] render_grid = getRenderGrid(leftUpTile, rightLowTile, depth);
+        results.put("render_grid", render_grid);
+
+        //add ullon, ullat, lrlat, lrlon of raster
+        results.putAll(rasterBounding(leftUpTile, rightLowTile, depth));
         return results;
+    }
+    private double deltaLonPerTile(int depth) {
+        return (MapServer.ROOT_LRLON - MapServer.ROOT_ULLON) / tilesNumInXOrY(depth);
+    }
+    private double deltaLatPerTile(int depth) {
+        return (MapServer.ROOT_LRLAT - MapServer.ROOT_ULLAT) / tilesNumInXOrY(depth);
+    }
+    // make x belong to [bound1, bound2]
+    private static double inBounding(double x, double bound1, double bound2) {
+        double min = Math.min(bound1, bound2);
+        double max = Math.max(bound1, bound2);
+        if (x < min){
+            return min;
+        } else if (x > max) {
+            return max;
+        }
+        return x;
+    }
+    private int[] whichTile(double lon, double lat, int depth) {
+        lon = inBounding(lon, MapServer.ROOT_ULLON, MapServer.ROOT_LRLON);
+        lat = inBounding(lat, MapServer.ROOT_ULLAT, MapServer.ROOT_LRLON);
+        int x = (int) Math.floor((lon - MapServer.ROOT_ULLON) / deltaLonPerTile(depth));
+        int y = (int) Math.floor((lat - MapServer.ROOT_ULLAT) / deltaLatPerTile(depth));
+        return new int[]{x, y};
+    }
+    private Map<String, Double> rasterBounding(int[] leftUpTile, int[] rightLow, int depth) {
+        double deltaLon = deltaLonPerTile(depth);
+        double deltaLat = deltaLatPerTile(depth);
+        double raster_ul_lon = MapServer.ROOT_ULLON + deltaLon * leftUpTile[0];
+        double raster_ul_lat = MapServer.ROOT_ULLAT + deltaLat * leftUpTile[1];
+        double raster_lr_lon = MapServer.ROOT_LRLON + deltaLon * (rightLow[0] + 1);
+        double raster_lr_lat = MapServer.ROOT_LRLAT + deltaLat * (rightLow[1] + 1);
+        Map<String, Double> bounding = new HashMap<>();
+        bounding.put("raster_ul_lon", raster_ul_lon);
+        bounding.put("raster_ul_lat", raster_ul_lat);
+        bounding.put("raster_lr_lon", raster_lr_lon);
+        bounding.put("raster_lr_lat", raster_lr_lat);
+        return bounding;
+    }
+
+    private String[][] getRenderGrid(int[] leftUpTile, int[] rightLowTile, int depth) {
+        // 0 stands for x-axis; 1 stands for y-axis
+        int xNumGrid = rightLowTile[0] - leftUpTile[0] + 1;
+        int yNumGrid = rightLowTile[1] - leftUpTile[1] + 1;
+        String[][] renderGrid = new String[xNumGrid][yNumGrid];
+        for (int x = leftUpTile[0]; x <= rightLowTile[0]; x++) {
+            for (int y = leftUpTile[1]; y <= rightLowTile[1]; y++) {
+                String fileName = "d" + depth + "_x" + x + "_y" + y + ".png";
+                int xRenderGrid = x - leftUpTile[0];
+                int yRenderGrid = y - leftUpTile[1];
+                renderGrid[xRenderGrid][yRenderGrid] = fileName;
+            }
+        }
+        return renderGrid;
+    }
+    private boolean inMap(double lon, double lat) {
+        boolean inLon = (lon >= MapServer.ROOT_ULLON) && (lon <= MapServer.ROOT_LRLON);
+        boolean inLat = (lat >= MapServer.ROOT_LRLON) && (lon <= MapServer.ROOT_ULLAT);
+        return !inLat || !inLon;
+    }
+    private double lonDPP(double ullon, double lrlon, double width) {
+        return (lrlon - ullon) / width;
+    }
+    private int tilesNumInXOrY(int depth) {
+        return (int) Math.pow(2, depth);
+    }
+    private double lonDPP(int depth) {
+        return lonDPP0 / tilesNumInXOrY(depth);
+    }
+    private int bestDepth(double ullon, double lrlon, double width) {
+        double lonDPPQuery = lonDPP(ullon, lrlon, width);
+        for (int depth = 0; depth < MapServer.MAX_DEPTH + 1; depth++) {
+            double lonDPPDepth = lonDPP(depth);
+            if (lonDPPDepth < lonDPPQuery) {
+                return depth;
+            }
+        }
+        return MapServer.MAX_DEPTH;
     }
 
 }
