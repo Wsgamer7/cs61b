79d41956b7b040b6fd61ca96f04ede87319b3362
Wsgamer <ws18022@outlook.com>
2023-04-27 22:53:28 +0800

    until test I

--- hw7/BinaryTrie.java
+++ hw7/BinaryTrie.java
@@ -1,6 +1,7 @@
 import edu.princeton.cs.algs4.MinPQ;
 
 import java.io.Serializable;
+import java.util.HashMap;
 import java.util.Map;
 
 public class BinaryTrie implements Serializable {
@@ -28,10 +29,40 @@ public class BinaryTrie implements Serializable {
         return pq.delMin();
     }
     public Match longestPrefixMatch(BitSequence querySequence) {
-
+        BitSequence longestPrefix = new BitSequence();
+        Node pointer = trie;
+        char ch = '\0';
+        for (int i = 0; i < querySequence.length(); i++) {
+            int bit = querySequence.bitAt(i);
+            ch = pointer.ch;
+            if (bit == 0) {
+                pointer = pointer.left;
+            } else {
+                pointer = pointer.right;
+            }
+            if (pointer == null) {
+                break;
+            }
+            longestPrefix.appended(bit);
+        }
+        return new Match(longestPrefix, ch);
     }
     public Map<Character, BitSequence> buildLookupTable() {
-
+        Map<Character, BitSequence> lookupTable = new HashMap<>();
+        lookupTableHelper(trie, new BitSequence(), lookupTable);
+        return lookupTable;
+    }
+    private void lookupTableHelper(Node node, BitSequence prefix,
+                                   Map<Character, BitSequence> lookupTable) {
+        if (node == null) {
+            return;
+        } else if (node.isLeaf()) {
+            char ch = node.ch;
+            lookupTable.put(ch, prefix);
+            return;
+        }
+        lookupTableHelper(node.left, prefix.appended(0), lookupTable);
+        lookupTableHelper(node.right, prefix.appended(1), lookupTable);
     }
     private static class Node implements Comparable<Node> {
         private final char ch;
@@ -46,6 +77,9 @@ public class BinaryTrie implements Serializable {
         @Override
         public int compareTo(Node that) {
             return this.freq - that.freq;
+        }
+        private boolean isLeaf() {
+            return left == null && right == null;
         }
     }
 }
