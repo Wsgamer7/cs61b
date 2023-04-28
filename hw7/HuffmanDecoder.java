6bade582a9bda099b6b47b7ef13569ae93ffa2c0
Wsgamer <ws18022@outlook.com>
2023-04-28 13:37:04 +0800

    modify reduce first N bit In bitsequence

--- hw7/HuffmanDecoder.java
+++ hw7/HuffmanDecoder.java
@@ -1,7 +1,7 @@
 public class HuffmanDecoder {
     public static void main(String[] args) {
         if (args.length != 2) {
-            System.out.println("Usage: HuffmanDecoder watermelonsugar.txt.huf originalwatermelon.txt");
+            System.out.println("Usage: HuffmanDecoder composed.txt.huf original.txt");
             return;
         }
         String fileToDecode = args[0];
@@ -19,7 +19,7 @@ public class HuffmanDecoder {
             Match match = binaryTrie.longestPrefixMatch(toDecode);
             symbols[i] = match.getSymbol();
             int matchSeqLength = match.getSequence().length();
-            toDecode.removeFirstN(matchSeqLength);
+            toDecode = toDecode.allButFirstNBits(matchSeqLength);
         }
         FileUtils.writeCharArray(fileToSave, symbols);
     }
